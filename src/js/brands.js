8f845a1ec71e99ca8cb030c742dd1529ed4e88d0
Daniil Fedorov <df.frontdevelop@gmail.com>
2023-01-08 13:27:44 +0100

    build: added src folder

---
+++ src/js/brands.js
@@ -0,0 +1,14 @@
+const brandsList = document.querySelector('.brands__list');
+const buttonExpand = document.getElementById('brends__expand');
+
+buttonExpand.addEventListener('click', function () {
+  if (brandsList.classList.contains('brands__list--visible')) {
+    brandsList.classList.remove('brands__list--visible');
+    buttonExpand.classList.remove('main-btn--open');
+    buttonExpand.textContent = 'Показать все';
+  } else {
+    brandsList.classList.add('brands__list--visible');
+    buttonExpand.classList.add('main-btn--open');
+    buttonExpand.textContent = 'Скрыть';
+  }
+});

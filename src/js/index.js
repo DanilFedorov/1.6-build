232c4c3718b2ee56fd104f179371015b9a0d836c
Daniil Fedorov <df.frontdevelop@gmail.com>
2023-01-10 16:26:58 +0100

    dev: error on build by MiniCssExtractPlugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-import '../style/style.css';
+import Post from "./post.js";
+import "../style/style.css";
+
+const post = new Post("Webpack Post Title");
+
+console.log("Post to String", post.toString());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,72 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
 module.exports = {
-    entry: './src/js/index/js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/js", "index.js"),
+  output: {
+    filename: "[name].[hash].js",
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+    sourceMapFilename: "[file].map",
+    chunkFilename: "[id].chunk.js",
+    assetModuleFilename: "./assets/[name].[ext]",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimize: isProd,
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerPlugin()],
+  },
+  devServer: {
+    port: 4200,
+    static: {
+      directory: path.join(__dirname, "/dist"),
+    },
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: path.resolve(__dirname, "dist/index.html"),
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ttf|woff|woff2|eot)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
